1ea29b469a3bc0cab96c0a7a39cde4938107ac17
SouryaS <136927034+SouryaS@users.noreply.github.com>
2025-05-21 05:08:43 +0530

    a

--- pneumonia-api/copy_model.ps1
+++
@@ -1,3 +0,0 @@
-# PowerShell script to copy the pneumonia model to the API directory
-Copy-Item -Path "../pneumonia_detection_model.h5" -Destination "./" -Force
-Write-Host "Copied pneumonia_detection_model.h5 to the pneumonia-api directory"
